b24a1721bf32194d8aea47d3fe34d96384672898
gregorygregory745 <psaccesargregory@gmail.com>
2024-08-24 02:15:03 -0400

    Add files via upload

---
+++ Fnac.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Fnac
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingresa tu fecha:");
+        string fechaNacimientoStr = Console.ReadLine();
+
+        try
+        {
+            DateTime fechaNacimiento = DateTime.ParseExact(fechaNacimientoStr, "dia/mes/año", null);
+            DayOfWeek diaDeLaSemana = fechaNacimiento.DayOfWeek;
+            Console.WriteLine("Naciste un día: " + diaDeLaSemana);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("ingresala como s debe");
+        }
+    }
+}
