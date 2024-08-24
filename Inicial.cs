324ecdddf25b84046d48bc6888fa6202233061fb
gregorygregory745 <psaccesargregory@gmail.com>
2024-08-24 02:22:04 -0400

    Add files via upload

---
+++ Inicial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Inicial
+{
+    static void Main()
+    {
+        Console.Write("Ingresa tu nombre: ");
+        string nombre = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(nombre))
+        {
+            char inicial = nombre[0];
+            Console.WriteLine("Tu inicial es: " + inicial);
+        }
+        else
+        {
+            Console.WriteLine("No ingresaste un nombre.");
+        }
+    }
+}
